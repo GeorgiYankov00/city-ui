3e95654383f83b7b05e8072e0b39f833ca3a6c58
unknown <georgi.yankov00@gmail.com>
2023-11-13 20:02:59 +0200

    feat: added sort using API params

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridSortModel,
+} from "@mui/x-data-grid";
+import { useCallback, useEffect, useState } from "react";
 import { API_URL, HIGH_POPULATION } from "./utils/const";
+import { Box } from "@mui/material";
+import { maxHeight } from "@mui/system";
 
 const columns: GridColDef[] = [
   { field: "name", headerName: "City", flex: 1 },
@@ -22,18 +29,42 @@ const columns: GridColDef[] = [
 export default function App() {
   const [rows, setRows] = useState<GridRowsProp>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortModel, setSortModel] = useState<GridSortModel>([]);
 
-  useEffect(() => {
-    fetch(API_URL)
+  const fetchData = useCallback(() => {
+    setIsLoading(true);
+
+    let queryUrl = API_URL;
+
+    //to do extract into
+    if (sortModel.length > 0) {
+      const sortField = sortModel[0].field;
+      const sortOrder = sortModel[0].sort === "asc" ? "" : "-";
+      const sortParam = `${sortOrder}${sortField}`;
+      queryUrl += `?sort=${sortParam}`;
+    }
+
+    fetch(queryUrl)
       .then((response) => response.json())
       .then((data) => {
         setRows(data);
         setIsLoading(false);
       });
-  }, []);
+  }, [sortModel]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
-    <div className="App" style={{ minHeight: 200 }}>
-      <DataGrid rows={rows} columns={columns} loading={isLoading} />
-    </div>
+    <Box sx={{ height: 500, width: 1 }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        sortModel={sortModel}
+        onSortModelChange={(model) => setSortModel(model)}
+        loading={isLoading}
+      />
+    </Box>
   );
 }
